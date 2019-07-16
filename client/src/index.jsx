c606bdd99abb0df22fba4920a5b1cf26c6e6a6db
Tommy Liao <tommyliao340@hotmail.com>
2019-07-15 23:03:21 -0700

    [feat] Preliminary setup

---
+++ client/src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render('hello world', document.getElementById('main'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './client/src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    ]
+  },
+  mode: 'development'
+};
